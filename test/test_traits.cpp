e7a10d4d1dbbc3d40457c38c37a8f5a9b935d351
Malegkin <malegkin@gmail.com>
2018-11-03 00:41:06 +0300

    add test for to_tuple foo

---
+++ test/test_traits.cpp
@@ -0,0 +1,45 @@
+
+#include "stdafx.h"
+#include "traits.h"
+
+#define BOOST_TEST_MODULE test_matrix
+#include <boost/test/unit_test.hpp>
+
+#define LOG_LEVEL all
+
+BOOST_AUTO_TEST_SUITE( test_traits )
+
+    BOOST_AUTO_TEST_CASE( test_traits__array_to_tuple ) {
+
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 0>()) == std::tuple<>() );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 1>({0})) == std::make_tuple(0) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 2>({0, 1})) == std::make_tuple(0, 1) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 3>({0, 1, 2 })) == std::make_tuple(0, 1, 2) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 4>({0, 1, 2, 3 })) == std::make_tuple(0, 1, 2, 3) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 5>({0, 1, 2, 3, 4 })) == std::make_tuple(0, 1, 2, 3, 4) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 6>({0, 1, 2, 3, 4, 5 })) == std::make_tuple(0, 1, 2, 3, 4, 5) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 7>({0, 1, 2, 3, 4, 5, 6})) == std::make_tuple(0, 1, 2, 3, 4, 5, 6) );
+        BOOST_REQUIRE( to_tuple(std::array<uint32_t, 8>({0, 1, 2, 3, 4, 5, 6, 7})) == std::make_tuple(0, 1, 2, 3, 4, 5, 6, 7));
+
+
+
+        std::array<uint32_t, 1024> arr;
+        uint32_t i = 0;
+        std::generate_n( begin(arr), 1024, [&](){return i++;});
+
+        auto t = to_tuple(arr);
+
+        BOOST_REQUIRE(8 ==  std::get<8>(t));
+        BOOST_REQUIRE(16 ==  std::get<16>(t));
+        BOOST_REQUIRE(32 ==  std::get<32>(t));
+        BOOST_REQUIRE(64 ==  std::get<64>(t));
+        BOOST_REQUIRE(128 ==  std::get<128>(t));
+        BOOST_REQUIRE(256 ==  std::get<256>(t));
+        BOOST_REQUIRE(512 ==  std::get<512>(t));
+        //BOOST_REQUIRE(1024 ==  std::get<1024>(t));
+        //compile error static_assert failed "tuple_element index out of range"
+        //    static_assert(_Ip < sizeof...(_Types), "tuple_element index out of range");
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
